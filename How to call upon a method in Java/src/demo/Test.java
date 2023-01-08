4125231d5719f5d369326a0aeb8caf822683077e
milky <milky@Milky-s_PC.lan>
2023-01-08 17:36:39 +0000

    First step

---
+++ How to call upon a method in Java/src/demo/Test.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Test {
+
+	static int tall = 31;
+	
+	public static void main(String[] args) {
+		System.out.println(isEmpty());
+	}
+	
+	public static boolean isEmpty() {
+		return tall==0;
+	}
+}
---
+++ How to call upon a method in Java/src/demo_II/How_to_call_upon_a_method_containing_string_in_java.java
@@ -0,0 +1,10 @@
+package demo_II;
+
+public class How_to_call_upon_a_method_containing_string_in_java {
+
+	static String tall = "";
+	public static void main(String[] args) {
+		System.out.println(tall.isEmpty());
+	}
+
+}
